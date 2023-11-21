4debabdcebec94830f2a59f988d0f43c4bd6d366
Robin <robin.bechlem@gmail.com>
2023-11-21 21:58:22 +0100

    rebase du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogueComponent
   ],
   imports: [
     BrowserModule
