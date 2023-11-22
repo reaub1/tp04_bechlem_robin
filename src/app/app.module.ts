ec4d174819e6365ba2c687f57cdb87f5782e4835
Robin <robin.bechlem@gmail.com>
2023-11-22 20:46:53 +0100

    Ajout des routes dans le projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { RootComponent } from './root/root.component';
+import { AppComponent } from './app.component';
 import { TetiereComponent } from './tetiere/tetiere.component';
 import { FooterComponent } from './footer/footer.component';
 
@@ -11,11 +11,21 @@ import { CatalogueModule } from './catalogue/catalogue.module';
 import { PanierComponent } from './panier/panier.component';
 import { StoreModule } from '@ngrx/store';
 import { PanierState } from './shared/states/panier-state';
+import { ProduitsComponent } from './produits/produits.component';
+
+
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: ProduitsComponent},
+  { path: 'panier', component: PanierComponent},
+  { path: 'produits', component: ProduitsComponent },
+];
 
 @NgModule({
-  imports: [BrowserModule, CatalogueModule, NgxsModule.forRoot([PanierState])],
-  declarations: [RootComponent, TetiereComponent, FooterComponent, PanierComponent],
-  bootstrap: [RootComponent],
+  imports: [BrowserModule, CatalogueModule, NgxsModule.forRoot([PanierState]), RouterModule.forRoot(routes)],
+  declarations: [AppComponent, TetiereComponent, FooterComponent, PanierComponent],
+  bootstrap: [AppComponent],
 })
 
 export class AppModule {}
