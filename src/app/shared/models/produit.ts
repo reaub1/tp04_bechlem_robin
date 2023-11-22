788a1a2e9c857d012282195b66a0bd1d585ccaea
Robin <robin.bechlem@gmail.com>
2023-11-22 18:37:04 +0100

    Réglage du style du projet

--- src/app/shared/models/produit.ts
+++ src/app/shared/models/produit.ts
@@ -1,10 +1,12 @@
 export class Produit {
+    url : string;
     ref: string;
     designation: string;
     prix: number;
     qte: number;
   
     constructor() {
+      this.url='';
       this.ref = '';
       this.designation = '';
       this.prix = 0;
